5e5da975bd2029190f177d48357182f0538495f0
Jit Rakshit <80247484+Jit-Rakshit@users.noreply.github.com>
2022-02-08 21:52:11 +0530

    Update App.java

--- src/main/java/myapp/App.java
+++ src/main/java/myapp/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("hey Jit");
+System.out.print("hey Jit testing new one ");
 	}
 
 }
